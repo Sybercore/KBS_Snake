08663b7ecccc5cdf7ef50807a22f91802430cd58
Xendrit <s1088203@student.windesheim.nl>
2017-04-21 12:55:57 +0200

    Fucntion decodeKeyboard added

--- C-code/hello_ucosii.c
+++ C-code/hello_ucosii.c
@@ -1,63 +1,67 @@
 #include <stdio.h>
 #include "includes.h"
-#include <altera_up_avalon_ps2.h>
 
 /* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
-OS_STK    read_keyboard_stk[TASK_STACKSIZE];
+OS_STK    readKeyboard_stk[TASK_STACKSIZE];
 
 /* Definition of Task Priorities */
 
 #define READ_KEYBOARD_PRIORITY      1
 
-void read_keyboard(void* pdata);
+void readKeyboard(void* pdata);
 
-void read_keyboard(void* pdata)
+char decodeKeyboard(int byte){
+	char key;
+	switch(byte)
+	{
+		/* Movement keys */
+		case 0x1D:
+			key = 'w';
+			break;
+		case 0x1C:
+			key = 'a';
+			break;
+		case 0x1B:
+			key = 's';
+			break;
+		case 0x23:
+			key = 'd';
+			break;
+		case 0x6B:
+			// 4
+			key = 'l';
+			break;
+		case 0x73:
+			// 5
+			key = 'z';
+			break;
+		case 0x74:
+			// 6
+			key = 'r';
+			break;
+		case 0x75:
+			// 8
+			key = 'u';
+	}
+	return key;
+}
+
+void readKeyboard(void* pdata)
 {
 	volatile int * PS2_ptr = (int *) 0x10000100;
-	int PS2_data, RVALID, RAVAIL;
+	int PS2_data, RAVAIL;
 	int byte = 0;
-	char key;
+	char key = 0;
   while (1)
   {
     PS2_data = *(PS2_ptr);
     RAVAIL = (PS2_data & 0xFFFF0000) >> 16;
-    RVALID = PS2_data & 0x8000;
+    //RVALID = PS2_data & 0x8000;
     if (RAVAIL){
     	byte = PS2_data & 0xFF;
     	printf ("key: %d", byte);
-    	switch(byte)
-    	{
-    		/* LETTERS */
-    		case 0x1D:
-    			key = 'w';
-    			break;
-    		case 0x1C:
-    			key = 'a';
-    			break;
-    		case 0x1B:
-    			key = 's';
-    			break;
-    		case 0x23:
-    			key = 'd';
-    			break;
-    		case 0x6B:
-    			// 4
-    			key = 'l';
-    			break;
-    		case 0x73:
-    			// 5
-    			key = 'z';
-    			break;
-    		case 0x74:
-    			// 6
-    			key = 'r';
-    			break;
-    		case 0x75:
-    			// 8
-    			key = 'u';
-    	}
-
+    	key = decodeKeyboard(byte);
     	printf ("letter: %c\n", key);
 
     }
@@ -65,18 +69,16 @@ void read_keyboard(void* pdata)
   }
 }
 
-
-
 /* The main function creates two task and starts multi-tasking */
 int main(void)
 {
 
-  OSTaskCreateExt(read_keyboard,
+  OSTaskCreateExt(readKeyboard,
                   NULL,
-                  (void *)&read_keyboard_stk[TASK_STACKSIZE-1],
+                  (void *)&readKeyboard_stk[TASK_STACKSIZE-1],
                   READ_KEYBOARD_PRIORITY,
                   READ_KEYBOARD_PRIORITY,
-                  read_keyboard_stk,
+                  readKeyboard_stk,
                   TASK_STACKSIZE,
                   NULL,
                   0);
